f8537e9477ae9524b51ec0f967df6133a93c3d18
Janderson <91136966+JCompSci@users.noreply.github.com>
2022-02-03 09:05:50 -0600

    Add files via upload

---
+++ windowPanel.java
@@ -0,0 +1,9 @@
+public class windowPanel extends gameControl
+{
+   private enemy enemy1;
+   private enemy enemy2;
+   private enemy enemy3;
+   private enemy enemy4;
+   private enemy enemy5;
+   
+}  
