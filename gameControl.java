25f7e64da626337c2ab1d07b5b6ee74cb8dc0c3d
Janderson <91136966+JCompSci@users.noreply.github.com>
2022-02-04 16:03:46 -0600

    Add files via upload

---
+++ gameControl.java
@@ -0,0 +1,5 @@
+public class gameControl extends setScreen
+{
+   
+
+}
---
+++ windowPanel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class windowPanel extends gameControl
+{  
+   public windowPanel()
+   {
+      //checks file for imformation on panels
+      Scanner sc = new Scanner(new file("world1");
+      pattern pat = new pattern("enemy");
+   }
+   
+   public enemy spawnEnemies();
+   {
+      
+   }
+}  
