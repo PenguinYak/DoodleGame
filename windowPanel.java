8373e2d5010905ca834f679c389e479f758a0c3b
Janderson <91136966+JCompSci@users.noreply.github.com>
2022-02-04 16:03:19 -0600

    Delete windowPanel.java

--- windowPanel.java
+++
@@ -1,9 +0,0 @@
-public class windowPanel extends gameControl
-{
-   private enemy enemy1;
-   private enemy enemy2;
-   private enemy enemy3;
-   private enemy enemy4;
-   private enemy enemy5;
-   
-}  
